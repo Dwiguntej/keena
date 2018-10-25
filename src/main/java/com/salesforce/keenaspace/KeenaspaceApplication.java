3c6f7c4f2ea979a50ee7bcf579e012fe0b9a6a5b
achaganti <achaganti@salesforc.com>
2018-10-25 19:03:57 +0530

    removed initializer

--- src/main/java/com/salesforce/keenaspace/KeenaspaceApplication.java
+++ src/main/java/com/salesforce/keenaspace/KeenaspaceApplication.java
@@ -2,10 +2,9 @@ package com.salesforce.keenaspace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class KeenaspaceApplication extends SpringBootServletInitializer {
+public class KeenaspaceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KeenaspaceApplication.class, args);
