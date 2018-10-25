02222e419bd8e41df8e42b0ee2f4c1f58f0d4db8
n-rajani <nrajani@salesforce.com>
2018-10-25 20:56:02 +0530

    implemented getTeamMembers

--- src/main/java/com/salesforce/keenaspace/controllers/EmployeeController.java
+++ src/main/java/com/salesforce/keenaspace/controllers/EmployeeController.java
@@ -8,15 +8,22 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class EmployeeController {
 
     @Autowired
     EmployeeService employeeService;
 
-    @RequestMapping(value = "/getKeenaEmployeeDetails", method = RequestMethod.GET)
+    @RequestMapping(value = "/getEmployeeDetails", method = RequestMethod.GET)
     public Employee getKeenaEmployeeDetails(@RequestParam("emailId") String email) {
         return employeeService.findByEmail(email);
+    }
+
+    @RequestMapping(value = "/getTeamMembers", method = RequestMethod.GET)
+    public List<Employee> getKeenaTeamMembers(@RequestParam("managerId") int managerId) {
+        return employeeService.findAllByManagerId(managerId);
     }
 
 }
