fe2a31d9aaf689e1c3be35968da2db5334c22baf
dpalla <dpalla@salesforce.com>
2018-10-26 00:58:03 +0530

    Fixed Rest Issue

--- src/main/java/com/salesforce/keenaspace/entity/Employee.java
+++ src/main/java/com/salesforce/keenaspace/entity/Employee.java
@@ -1,9 +1,12 @@
 package com.salesforce.keenaspace.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "EMPLOYEE")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Employee {
 
   @Column(name = "EMPLOYEE_ID")
