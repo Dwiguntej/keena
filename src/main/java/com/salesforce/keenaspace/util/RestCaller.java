93e0913a514622e354c2d294d47716a28f48b546
vragi <vragi@salesforce.com>
2018-10-26 17:39:35 +0530

    Refactored RestCaller

--- src/main/java/com/salesforce/keenaspace/util/RestCaller.java
+++ src/main/java/com/salesforce/keenaspace/util/RestCaller.java
@@ -11,32 +11,29 @@ import java.net.URISyntaxException;
 
 public class RestCaller {
 
-    public static void main(String[] args) throws Exception {
-        getUserEmail();
-    }
+  public static void main(String[] args) throws Exception {
+    getUserEmail();
+  }
 
   public static String getUserEmail() throws Exception {
 
-    String urlParameters =
-        "?grant_type=password&client_id=3MVG9d3kx8wbPieF4xQoFOpXR2aRGDhd4WfEPWC3slu5il.H1ZJc5LuxDD_vpajQ2IiYdR.X3d3XdZrhcXaP5&client_secret=4868920179566624296&username=rnemani@salesforce.com.fy19hack1&password=$HackathonWork123";
-    String responseBody =
-        RestCaller.execute("https://test.salesforce.com/services/oauth2/token", urlParameters);
+    String targetURL =
+        "https://test.salesforce.com/services/oauth2/token?grant_type=password&client_id=3MVG9d3kx8wbPieF4xQoFOpXR2aRGDhd4WfEPWC3slu5il.H1ZJc5LuxDD_vpajQ2IiYdR.X3d3XdZrhcXaP5&client_secret=4868920179566624296&username=rnemani@salesforce.com.fy19hack1&password=$HackathonWork123";
+    String responseBody = RestCaller.execute(targetURL);
     ObjectMapper objectMapper = new ObjectMapper();
     JsonNode jsonNode = objectMapper.readTree(responseBody);
     String accessToken = jsonNode.get("access_token").asText();
-    String serviceResponse =
-        RestCaller.getUserDetails(
+    String serviceResponse = RestCaller.getUserDetails(
             jsonNode.get("instance_url").asText() + "/services/apexrest/getLoggedInUserDetails",
-            accessToken,
-            urlParameters);
-      objectMapper = new ObjectMapper();
-      jsonNode = objectMapper.readTree(responseBody);
-      return jsonNode.get("Email").asText();
+            accessToken);
+    objectMapper = new ObjectMapper();
+    jsonNode = objectMapper.readTree(serviceResponse);
+    return jsonNode.get("Email").asText();
   }
 
-  public static String execute(String targetURL, String urlParameters) {
+  public static String execute(String targetURL) {
     HttpHeaders headers = new HttpHeaders();
-    headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+    //    headers.setContentType(MediaType.MULTIPART_FORM_DATA);
 
     MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
 
@@ -44,13 +41,11 @@ public class RestCaller {
         new HttpEntity<MultiValueMap<String, String>>(map, headers);
 
     RestTemplate restTemplate = new RestTemplate();
-    ResponseEntity<String> response =
-        restTemplate.postForEntity(targetURL + urlParameters, entity, String.class);
+    ResponseEntity<String> response = restTemplate.postForEntity(targetURL, entity, String.class);
     return response.getBody();
   }
 
-  public static String getUserDetails(String url, String accessToken, String urlParams)
-      throws URISyntaxException {
+  public static String getUserDetails(String url, String accessToken) throws URISyntaxException {
     HttpHeaders headers = new HttpHeaders();
     headers.setContentType(MediaType.APPLICATION_JSON);
     headers.set("Authorization", "Bearer " + accessToken);
