fc66a9ba3b292b2b247bf08055750c0eb83617fe
dpalla <dpalla@salesforce.com>
2018-10-26 16:31:52 +0530

    Js Changes API integration

--- src/main/resources/static/js/keenaspace.js
+++ src/main/resources/static/js/keenaspace.js
@@ -13,8 +13,9 @@ var getAvailableSeats = function () {
                 tableBody = tableBody + "<tr>";
                 tableBody = tableBody + "<td>" + element.seatId + "</td>";
                 tableBody = tableBody + "<td>" + element.managerName + "</td>";
-                tableBody = tableBody + "<td><a class=\"ui-btn ui-corner-all\" onclick=reserveSeat(\"" + element.seatId + "\",36)>Reserve</a></td>";
+                tableBody = tableBody + "<td><a id=\"" + index + "\" class=\"ui-btn ui-corner-all\" onclick=reserveSeat(\"" + element.seatId + "\",36,\"" + index + "\")>Reserve</a></td>";
                 tableBody = tableBody + "</tr>";
+                index = index + 1;
             });
             $("#table-custom-2").append(tableBody);
             console.log(data);
@@ -25,18 +26,35 @@ var getAvailableSeats = function () {
     });
 };
 
-var reserveSeat = function (seatId, empId) {
-    $.ajax({
-        url: urls.availableSeatsLocation + "reserveSeat?empId=" + empId + "&seatId=" + seatId,
-        method: "POST",
-        success: function (data) {
+var reserveSeat = function (seatId, empId, index) {
+    if (document.getElementById(index).text == "Reserve") {
+        $.ajax({
+            url: urls.availableSeatsLocation + "reserveSeat?empId=" + empId + "&seatId=" + seatId,
+            method: "POST",
+            success: function (data) {
+                document.getElementById(index).text = "Un-Reserve";
+                document.getElementById(index).style.color = 'red';
+            },
+            error: function (e) {
+                console.log(e);
+            }
+        });
+    }
+    else {
 
-            console.log(data);
-        },
-        error: function (e) {
-            console.log(e);
-        }
-    });
+        $.ajax({
+            url: urls.availableSeatsLocation + "unReserveSeat?empId=" + empId + "&seatId=" + seatId,
+            method: "PUT",
+            success: function (data) {
+                document.getElementById(index).text = "Reserve";
+                document.getElementById(index).style.color = '';
+            },
+            error: function (e) {
+                console.log(e);
+            }
+        });
+    }
+
 }
 
 $(document).ready(function () {
