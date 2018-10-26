fe7fa60fc2efd57a9968b0f629c5ae3ad803d7c9
dpalla <dpalla@salesforce.com>
2018-10-26 13:46:52 +0530

    Fixed Rest Issue

--- src/main/resources/static/js/keenaspace.js
+++ src/main/resources/static/js/keenaspace.js
@@ -1,5 +1,5 @@
 var urls = {
-    availableSeatsLocation: "https://keena-space.herokuapp.com/"
+    availableSeatsLocation: "http://localhost:5000/seats"
 };
 
 var getAvailableSeats = function () {
@@ -9,13 +9,13 @@ var getAvailableSeats = function () {
         success: function (data) {
             var tableBody = "";
             data.forEach(function (element) {
-                tableBody = "<tr>";
-                tableBody = "<td>" + element.seatId + "</td>";
-                tableBody = "<td>" + element.managerName + "</td>";
-                tableBody = "<td><a class=\"ui-btn ui-corner-all\">Reserve</a></td>";
-                tableBody = "</tr>";
+                tableBody = tableBody + "<tr>";
+                tableBody = tableBody + "<td>" + element.seatId + "</td>";
+                tableBody = tableBody + "<td>" + element.managerName + "</td>";
+                tableBody = tableBody + "<td><a class=\"ui-btn ui-corner-all\">Reserve</a></td>";
+                tableBody = tableBody + "</tr>";
             });
-            $("#table-custom-2").html(tableBody);
+            $("#table-custom-2").append(tableBody);
             console.log(data);
         },
         error: function (e) {
