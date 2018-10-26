b05f0d422ff54d57c3357cef2af14ee85218b994
rakeshnemani <rnemani@salesforce.com>
2018-10-26 16:12:24 +0530

    added apply leave tab

--- src/main/resources/static/js/keenaspace.js
+++ src/main/resources/static/js/keenaspace.js
@@ -19,3 +19,43 @@ $(document).ready(function () {
     getAvailableSeats();
 
 });
+
+
+var changeAvailability = function (emailId) {
+    getEmployeeDetails(emailId);
+};
+var getEmployeeDetails = function (emailId) {
+    $.ajax({
+        url: urls.availableSeatsLocation + "getEmployeeDetails?emailId=" + emailId,
+        method: "GET",
+        success: function (data) {
+
+            console.log(data);
+            getTeamMembers(data.manager.id)
+        },
+        error: function (e) {
+            console.log(e);
+        }
+    });
+};
+
+var getTeamMembers = function (managerId) {
+    $.ajax({
+        url: urls.availableSeatsLocation + "getTeamMembers?managerId=" + managerId,
+        method: "GET",
+        success: function (data) {
+            var divBody = "";
+            data.forEach(function (element) {
+                divBody = divBody + "<div class=\"ui-radio ui-mini\">"
+                divBody = divBody + "<input type= \"radio\" name=\"radio-choice-v-" + element.id + "\" id=\"team-member-" + element.id + "\" value=\"" + element.id + "\">";
+                divBody = divBody + "<label for=\"team-member-" + element.id + "\" class=\"ui-btn ui-corner-all ui-btn-inherit ui-btn-icon-left ui-radio-off ui-last-child\">" + element.firstname + " " + element.lastname + "</label>";
+                divBody = divBody + "</input>";
+                divBody = divBody + "</div>";
+            });
+            $("#team-member").append(divBody);
+        },
+        error: function (e) {
+            console.log(e);
+        }
+    });
+};
